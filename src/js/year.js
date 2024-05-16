18c016a93792a61b086dfa438d02641be34004eb
KLS <kyliannlvs2@gmail.com>
2024-05-16 17:35:18 +0200

    Modification partie Export

--- src/js/year.js
+++ src/js/year.js
@@ -4,7 +4,6 @@ class Year {
 
 	constructor() {
 		this.loadYears();
-		this.addYear();
 	}
 
 	// Méthode pour charger les années
@@ -14,12 +13,12 @@ class Year {
 			url: 'http://localhost:8000/api/annee',
 			type: 'GET',
 			dataType: 'json',
-			success: function(data) {
-				$.each(data, function(index, annee) {
+			success: function(data){
+				$.each(data, function(index, annee){
 					$('.form-control').append('<option value="' + annee.id_annee + '">' + annee.annee + '</option>');
 				});
 			},
-			error: function(jqXHR, textStatus, errorThrown) {
+			error: function(jqXHR, textStatus, errorThrown){
 				console.log('Erreur : ' + errorThrown);
 			}
 		});
@@ -30,7 +29,8 @@ class Year {
 	addYear() {
 		$('.form-control').after('<button id="addYearBtn" class="btn btn-primary">Ajouter</button>');
 
-		$(document).on('click', '#addYearBtn', () => {
+		$(document).on('click', '#addYearBtn', function(){
+			// prendre le dernier élément de la liste
 			var beforeLastYear = $('.form-control option:nth-last-child(1)').text();
 			var yearParts = beforeLastYear.split('-');
 			var startYear = parseInt(yearParts[0]) + 1;
@@ -42,11 +42,12 @@ class Year {
 				url: 'http://localhost:8000/api/addAnnee',
 				type: 'POST',
 				dataType: 'json',
-				data: { annee: newYear },
-				success: function(data) {
+				data: JSON.stringify([{ annee: newYear }]),
+				success: function(data){
 					$('.form-control').append('<option value="' + data.id_annee + '">' + data.annee + '</option>');
 				},
-				error: function(jqXHR, textStatus, errorThrown) {
+				
+				error: function(jqXHR, textStatus, errorThrown){
 					console.log('Erreur : ' + errorThrown);
 				}
 			});
@@ -54,8 +55,7 @@ class Year {
 	}
 }
 
-// Instanciation de la classe Home
+// Instanciation de la classe Year
 
-$(document).ready(function() {
-	const year = new Year();
-});
+const year = new Year();
+year.addYear();
