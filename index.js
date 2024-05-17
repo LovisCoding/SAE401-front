8309c3eae726910a432dab95256c7f727ec71211
KLS <kyliannlvs2@gmail.com>
2024-05-17 16:51:26 +0200

    Modification et implémentation de l'Export

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(cors({origin: "*"}));
+app.use(cors({origin: true}));
 
 // sendFile will go here
 app.get('/', function(req, res) {
